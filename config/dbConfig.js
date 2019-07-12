69eeac2dc358c62a1701fc9e079d64ecb84070a6
dmitriyjd <dimon05091996abcd@mail.ru>
2019-07-12 15:39:10 +0300

    fix dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "1",
-    database: process.env.DB_NAME || "postgres",
+    username: process.env.DB_USER || "username",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_NAME || "dbName",
     host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_TYPE || "postgres"
   }
