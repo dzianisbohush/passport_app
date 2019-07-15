1e33ab8187cf97c7c7df7846cccef29855f7c286
Denis Bogush <dmbogush91@gmail.com>
2019-07-15 10:05:34 +0300

    Add structure for passwords/share for fe

--- src/common/api/sharePasswords.js
+++ src/common/api/sharePasswords.js
@@ -1,13 +1,30 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
 
+const createBodyForSharing = (userEmail, emails, passwordItems) => {
+  const records = [];
+
+  passwordItems.forEach(password => {
+    emails.forEach(email => {
+      records.push({
+        ...password,
+        userEmail: email,
+      });
+    });
+  });
+
+  return {
+    email: userEmail,
+    emailsToShare: emails,
+    records,
+  };
+};
+
 const sharePasswords = async (userEmail, emails, passwordItems) => {
   try {
-    const response = await axios.post('/api/passwords/share', {
-      userEmail,
-      emails,
-      passwordItems,
-    });
+    const body = createBodyForSharing(userEmail, emails, passwordItems);
+
+    const response = await axios.post('/api/passwords/share', body);
     return response;
   } catch (error) {
     console.log(error);
