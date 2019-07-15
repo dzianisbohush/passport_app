d982721926b364b430ba21d4d3a6fd73cf264720
Andrew <andy.yanushkevich@gmail.com>
2019-07-15 15:33:24 +0300

    small fix

--- src/server/controllers/files.js
+++ src/server/controllers/files.js
@@ -34,7 +34,6 @@ function adaptData(userEmail, originalData) {
 async function uploadFile(req, res) {
   const { userEmail } = req.body;
   const originalData = await asyncParseCSV(req.file.path);
-  console.log(originalData);
   const parsedResults = await adaptData(userEmail, originalData);
   parsedResults.forEach(item => {
     if (!item.userEmail) {
