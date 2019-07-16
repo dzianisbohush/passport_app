bc47adbad145c694c392a62444a671bf843e781a
andrewzb <rrr3122rrr3122>
2019-07-16 13:13:29 +0300

    logic for ext

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "1111",
+    username: process.env.DB_USER || "username",
+    password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_NAME || "passport",
     host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_TYPE || "postgres"
